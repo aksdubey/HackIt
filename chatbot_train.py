d1ed976b81b7ca25b921e00f538fe0b8888766d3
Akash Dubey <43945272+aksdubey@users.noreply.github.com>
2019-05-30 19:23:46 +0530

    Add files via upload

---
+++ chatbot_train.py
@@ -0,0 +1,56 @@
+from chatterbot import ChatBot
+#from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer
+import os
+
+chatbot = ChatBot(
+					'Training Example',
+#					storage_adapter='chatterbot.storage.SQLStorageAdapter',
+#					database_uri='sqlite:///database.sqlite3'
+				logic_adapters=[
+						'chatterbot.logic.MathematicalEvaluation',
+						#'chatterbot.logic.TimeLogicAdapter',
+					    {	
+							'import_path':'chatterbot.logic.BestMatch',
+							'default_response': 'I am sorry, but I do not understand.',
+							'maximum_similarity_threshold': 0.90
+					 }	
+    ]
+)
+
+trainer = ChatterBotCorpusTrainer(chatbot)
+
+trainer.train(
+    "C:/Users/ADMIN\chatterbot-corpus-master\chatterbot_corpus\data\english/"
+	#"./data/CUBE/"
+)
+# bot.trainer(ChatterBotCorpusTrainer)
+# bot.train("chatterbot.corpus.english")
+# for files in os.listdir('C:/Users/ADMIN\chatterbot-corpus-master\chatterbot_corpus\data\english/'):
+	# data = open('C:/Users/ADMIN\chatterbot-corpus-master\chatterbot_corpus\data\english/'+files, 'r').readlines()
+	# bot.train(data)
+
+# while True:
+		# message = input('You:')
+		# if message.strip()!='Bye':
+				# reply= chatbot.get_response(message)
+				# print('YAKSHA:',reply)
+
+		# if message.strip() == 'Bye':
+				# print('YAKSHA:Bye') 
+				# break
+# def setup():
+    # chatbot = ChatBot('Bot')
+    # storage_adapter=('chatterbot.storage.SQLStorageAdapter')
+    # trainer=('chatterbot.trainers.ListTrainer')
+    # corpus_path = 'C:/Users/ADMIN\chatterbot-corpus-master\chatterbot_corpus\data\english/'
+    # for file in os.listdir(corpus_path):
+    #    convData = open('C:/Users/ADMIN\chatterbot-corpus-master\chatterbot_corpus\data\english/' + file,'r').readlines()
+        #chatbot.set_trainer(ListTrainer)
+        #print("hello")
+        #trainer.train(corpus_path + file)
+        #trainer = ChatterBotCorpusTrainer(chatbot)
+        #chatbot.train(convData)
+        #trainer.train(convData)
+        #print("Training completed")
+#setup()
---
+++ tkinter_gui_Copy.py
@@ -0,0 +1,95 @@
+from chatterbot import ChatBot
+import tkinter as tk
+try:
+	import ttk as ttk
+	import ScrolledText
+except ImportError:
+	import tkinter.ttk as ttk
+	import tkinter.scrolledtext as ScrolledText
+import time, os
+import pyttsx3
+import speech_recognition as sr
+
+
+class TkinterGUIExample(tk.Tk):
+	def __init__(self, *args, **kwargs):
+		tk.Tk.__init__(self, *args, **kwargs)
+		self.chatbot = ChatBot(
+			"GUI Bot",#storage_adapter="chatterbot.storage.SQLStorageAdapter",
+			logic_adapters=[
+                "chatterbot.logic.BestMatch"
+            ],
+			#database_uri="sqlite:///database.db"
+		)
+		self.title("YAKSHA")
+		self.initialize()
+
+	def initialize(self):
+		self.grid()
+		self.respond = ttk.Button(self, text='Get Response', command=self.get_response)
+		self.respond.grid(column=0, row=0, sticky='nesw', padx=3, pady=3)
+		self.respond = ttk.Button(self, text='ASk question', command=self.process_speech)
+		self.respond.grid(column=0, row=1, sticky='nesw', padx=3, pady=3)
+		self.usr_input = ttk.Entry(self, state='normal')
+		self.usr_input.grid(column=1, row=0, sticky='nesw', padx=3, pady=3)
+		self.conversation_lbl = ttk.Label(self, anchor=tk.E, text='Conversation:')
+		self.conversation_lbl.grid(column=0, row=2, sticky='nesw', padx=3, pady=3)
+		self.conversation = ScrolledText.ScrolledText(self, state='disabled')
+		self.conversation.grid(column=0, row=3, columnspan=2, sticky='nesw', padx=3, pady=3)
+
+	def get_response(self, event=None):
+		user_input = self.usr_input.get()
+		self.say_out_loud(str(user_input), 1)
+		self.usr_input.delete(0, tk.END)
+		response = self.chatbot.get_response(user_input)
+		self.conversation['state'] = 'normal'
+		self.conversation.insert(			
+			tk.END, "You: " + user_input + "\n" + "Yaksha: " + str(response.text) + "\n"
+		)
+		self.say_out_loud(str(response.text), 0)
+		self.conversation['state'] = 'disabled'
+		
+	def say_out_loud(self, text_speech, male_or_female):
+		engine = pyttsx3.init() # object creation
+		""" RATE"""
+		rate = engine.getProperty('rate')   # getting details of current speaking rate
+		print (rate)                        #printing current voice rate
+		engine.setProperty('rate', 125)     # setting up new voice rate
+		"""VOLUME"""
+		volume = engine.getProperty('volume')   #getting to know current volume level (min=0 and max=1)
+		print (volume)                          #printing current volume level
+		engine.setProperty('volume',1.0)    # setting up volume level  between 0 and 1
+		"""VOICE"""
+		voices = engine.getProperty('voices')       #getting details of current voice
+		if male_or_female is 0:
+			engine.setProperty('voice', voices[0].id)  #changing index, changes voices. o for male
+		elif male_or_female is 1:
+			engine.setProperty('voice', voices[1].id)   #changing index, changes voices. 1 for female
+		engine.say(text_speech)
+		engine.runAndWait()
+		engine.stop()
+	
+	def process_speech(self):
+		r = sr.Recognizer()
+		mic = sr.Microphone()
+		with mic as source:
+			r.adjust_for_ambient_noise(source)
+			audio = r.listen(source)
+		print('speech ennds')
+		speech_text = r.recognize_google(audio)
+		print('speech text' + str(speech_text))
+		self.say_out_loud(str(speech_text), 1)
+		self.usr_input.delete(0, tk.END)
+		response = self.chatbot.get_response(str(speech_text))
+		self.conversation['state'] = 'normal'
+		self.conversation.insert(			
+			tk.END, "You: " + str(speech_text) + "\n" + "Yaksha: " + str(response.text) + "\n"
+		)
+		self.say_out_loud(str(response.text), 0)
+		self.conversation['state'] = 'disabled'
+
+
+gui_example = TkinterGUIExample()
+gui_example.bind('<Return>', gui_example.get_response)
+gui_example.mainloop()
+			
